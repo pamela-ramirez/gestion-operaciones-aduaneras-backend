7a5fcf95c6b537f99262ff77134e44bf07fff232
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-26 19:59:32 -0300

    fix: correccion de atributo context, y correccion de metodos para usar FindById

--- LogicaAccesoDatos/Repositorios/RepositorioUsuario.cs
+++ LogicaAccesoDatos/Repositorios/RepositorioUsuario.cs
@@ -1,6 +1,7 @@
 ﻿using LogicaAccesoDatos.Contexto;
 using LogicaNegocio.Entidades;
 using LogicaNegocio.InterfacesRepositorios;
+using LogicaNegocio.ValueObject;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,63 +13,63 @@ namespace LogicaAccesoDatos.Repositorios
 {
     public class RepositorioUsuario : IRepositorioUsuario
     {
-        public GestionOperacionesDbContext Contexto { get; set; }
+        //public GestionOperacionesDbContext Contexto { get; set; }
+        private readonly GestionOperacionesDbContext _context;
 
-        public RepositorioUsuario(GestionOperacionesDbContext contexto)
+        public RepositorioUsuario(GestionOperacionesDbContext context)
         {
-            Contexto = contexto;
+            _context = context;
         }
-
 
         public void Add(Usuario item)
         {
-            Contexto.Usuarios.Add(item);
-            Contexto.SaveChanges();
-
+            _context.Usuarios.Add(item);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            var usuario = Contexto.Usuarios.Find(id);
-            if (usuario != null)
+            var usuario = FindById(id);
+            if (usuario == null)
             {
-                Contexto.Usuarios.Remove(usuario);
-                Contexto.SaveChanges();
+                throw new Exception("Usuario no encontrado.");
             }
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
         }
 
         // Traer todos los usuarios
         public IEnumerable<Usuario> FindAll()
         {
-            return Contexto.Usuarios
+            return _context.Usuarios
                 .Include(u => u.Rol)
                 .ToList();
         }
 
         public Usuario FindById(int id)
         {
-            return Contexto.Usuarios
+            return _context.Usuarios
                 .Include(u => u.Rol)
                 .FirstOrDefault(u => u.Id == id);
         }
 
         public async Task<Usuario?> GetByEmail(string email)
         {
-            return await Contexto.Usuarios // Accede a la tabla de usuarios
+            return await _context.Usuarios // Accede a la tabla de usuarios
            .Include(u => u.Rol) // Asegura que el rol se cargue junto con el usuario
            .FirstOrDefaultAsync(u => u.Email.Valor == email); // Busca el usuario por su email
         }
 
         public void Update(Usuario item, int id)
         {
-            var usuarioExistente = Contexto.Usuarios.Find(id);
-            if(usuarioExistente != null)
+            var usuarioExistente = FindById(id);
+            if (usuarioExistente != null)
             {
                 usuarioExistente.Nombre = item.Nombre;
                 usuarioExistente.Apellido = item.Apellido;
-                usuarioExistente.Email = item.Email;
+                usuarioExistente.Email = new Email(item.Email.Valor);
                 usuarioExistente.RolId = item.RolId;
-                Contexto.SaveChanges();
+                _context.SaveChanges();
             }
         }
     }
