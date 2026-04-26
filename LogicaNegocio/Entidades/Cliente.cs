6f6b3244168352a2831c89e03f6ae549bc07fa81
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-26 15:09:02 -0300

    fix: Agrego atributos direccion y telefono segun RF-04

--- LogicaNegocio/Entidades/Cliente.cs
+++ LogicaNegocio/Entidades/Cliente.cs
@@ -10,12 +10,17 @@ namespace LogicaNegocio.Entidades
     public class Cliente:Usuario
     {
         public string Rut { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string? Direccion { get; set; }
+
 
         public Cliente() { }
 
-        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, string rut) : base(nombre, apellido, email, password, rol)
+        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, string rut, string telefono, string? direccion) : base(nombre, apellido, email, password, rol)
         {
             this.Rut = rut;
+            this.Telefono = telefono;
+            this.Direccion = direccion;
         }
 
 
