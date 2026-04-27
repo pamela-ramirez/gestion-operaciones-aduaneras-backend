2494f1d934b7b3fadb8c8c3737fe4bee233f1808
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-27 00:09:25 -0300

    feat: creo servicio de cliente

---
+++ GestionOperacionesAduaneras/Servicios/ClienteService.cs
@@ -0,0 +1,94 @@
+﻿using Compartido.DTOs.Cliente;
+using LogicaNegocio.Entidades;
+using LogicaNegocio.InterfacesRepositorios;
+using LogicaNegocio.InterfacesServicios;
+using LogicaNegocio.ValueObject;
+
+namespace GestionOperacionesAduaneras.Servicios
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IRepositorioCliente _repositorioCliente;
+
+        public ClienteService(IRepositorioCliente repositorioCliente)
+        {
+            _repositorioCliente = repositorioCliente;
+        }
+
+        public ClienteRespuestaDTO CrearCliente(CrearClienteDTO dto)
+        {
+            var cliente = new Cliente
+            {
+                Nombre = dto.Nombre,
+                Apellido = dto.Apellido,
+                Email = new Email(dto.Email),
+                Password = new Password(dto.Password),
+                Rut = dto.Rut,
+                Telefono = dto.Telefono,
+                Direccion = dto.Direccion
+            };
+            _repositorioCliente.Add(cliente);
+            return MapearARespuesta(cliente);
+        }
+
+        public void EliminarCliente(int id)
+        {
+            var cliente = _repositorioCliente.FindById(id);
+
+            if (cliente == null)
+                throw new Exception("Cliente no encontrado.");
+
+            _repositorioCliente.Delete(id);
+
+        }
+
+        public ClienteRespuestaDTO ModificarCliente(int id, ModificarClienteDTO dto)
+        {
+            var cliente = _repositorioCliente.FindById(id);
+            if (cliente == null)
+            {
+                throw new Exception("Cliente no encontrado");
+            }
+            cliente.Nombre = dto.Nombre;
+            cliente.Apellido = dto.Apellido;
+            cliente.Email = new Email(dto.Email);
+            cliente.Rut = dto.Rut;
+            cliente.Telefono = dto.Telefono;
+            cliente.Direccion = dto.Direccion;
+
+            _repositorioCliente.Update(cliente, id);
+            return MapearARespuesta(cliente);
+        }
+
+        public ClienteRespuestaDTO ObtenerClientePorId(int id)
+        {
+            var cliente = _repositorioCliente.FindById(id);
+            if (cliente == null)
+            {
+                throw new Exception("Cliente no encontrado");
+            }
+            return MapearARespuesta(cliente);
+        }
+
+        public IEnumerable<ClienteRespuestaDTO> ObtenerClientes()
+        {
+            var clientes = _repositorioCliente.FindAll();
+            return clientes.Select(c => MapearARespuesta(c)).ToList();
+
+        }
+        private ClienteRespuestaDTO MapearARespuesta(Cliente cliente)
+        {
+            return new ClienteRespuestaDTO
+            {
+                Id = cliente.Id,
+                Nombre = cliente.Nombre,
+                Apellido = cliente.Apellido,
+                Email = cliente.Email.Valor,
+                Rut = cliente.Rut,
+                Telefono = cliente.Telefono,
+                Direccion = cliente.Direccion ?? string.Empty
+                
+            };
+        }
+    }
+}
