829b9d2dd68e391ffb4eb6cd84c64556bdb85b38
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-28 20:39:24 -0300

    feat: añado implemnetacion y corrijo nombres de casos de uso usuario

--- LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Usuarios/EliminarUsuario.cs
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Usuarios/EliminarUsuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Usuarios;
+using LogicaNegocio.InterfacesServicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Usuarios
 {
-    internal class EliminarUsuario
+    public class EliminarUsuario : IEliminarUsuario
     {
+        private readonly IUsuarioService _usuarioService;
+
+        public EliminarUsuario(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+        public void Ejecutar(int id)
+        {
+            _usuarioService.EliminarUsuario(id);
+        }
     }
 }
