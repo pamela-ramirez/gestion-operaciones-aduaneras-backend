fd148ac9132ccdf6f142cb5b659a0ee1dd699cf6
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-28 00:01:02 -0300

    feat: creo repo e interfaz de rol para usarla el crear cliente

---
+++ LogicaAccesoDatos/Repositorios/RepositorioRol.cs
@@ -0,0 +1,66 @@
+﻿using LogicaAccesoDatos.Contexto;
+using LogicaNegocio.Entidades;
+using LogicaNegocio.Excepciones.Roles;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Repositorios
+{
+    public class RepositorioRol : IRepositorioRol
+    {
+        private readonly GestionOperacionesDbContext _context;
+
+        public RepositorioRol(GestionOperacionesDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Rol item)
+        {
+            _context.Roles.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var rol = FindById(id);
+            if (rol == null)
+            {
+                throw new RolException();
+            }
+            _context.Roles.Remove(rol);
+            _context.SaveChanges();
+        }
+
+        public void Update(Rol item, int id)
+        {
+            var rolExistente = FindById(id);
+            if (rolExistente == null)
+            {
+                throw new RolException();
+            }
+
+            rolExistente.NombreRol = item.NombreRol;
+            _context.SaveChanges();
+        }
+
+        public Rol FindById(int id)
+        {
+            return _context.Roles.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Rol> FindAll()
+        {
+            return _context.Roles.ToList();
+        }
+
+        public Rol FindByNombre(string nombreRol)
+        {
+            return _context.Roles.FirstOrDefault(r => r.NombreRol == nombreRol);
+        }
+    }
+}
--- LogicaNegocio/InterfacesRepositorios/IRepositorioRol.cs
+++ LogicaNegocio/InterfacesRepositorios/IRepositorioRol.cs
@@ -9,6 +9,6 @@ namespace LogicaNegocio.InterfacesRepositorios
 {
     public interface IRepositorioRol:IRepositorio<Rol>
     {
-
+        Rol FindByNombre(string nombreRol);
     }
 }
