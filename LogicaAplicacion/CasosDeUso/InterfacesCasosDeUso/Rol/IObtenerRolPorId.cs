a20b6681d650a955cadc5671dfce5c5bcdc2d273
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-29 00:38:30 -0300

    feat: agrego interfaces de CU rol

---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/Rol/IObtenerRolPorId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.Rol.RolDTO;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Rol
+{
+    public interface IObtenerRolPorId
+    {
+        RolRespuestaDTO Ejecutar(int id);
+    }
+}
