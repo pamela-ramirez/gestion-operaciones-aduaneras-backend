740edf41bce7a8208ea69b98afc21bb08c548b74
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-29 00:29:48 -0300

    fix: agrego nuevamente exception roll no encontrado y la coloco en servicio

--- GestionOperacionesAduaneras/Servicios/ClienteService.cs
+++ GestionOperacionesAduaneras/Servicios/ClienteService.cs
@@ -23,7 +23,7 @@ namespace GestionOperacionesAduaneras.Servicios
         public ClienteRespuestaDTO CrearCliente(CrearClienteDTO dto)
         {
             var rolCliente = _repositorioRol.FindByNombre("Cliente")
-                ?? throw new RolException();
+                ?? throw new RolNoEncontradoException();
 
             var cliente = new Cliente
             {
