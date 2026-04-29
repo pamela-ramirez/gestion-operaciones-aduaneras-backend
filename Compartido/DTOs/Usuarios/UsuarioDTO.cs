858babf3df12fc7eb3ed44338c752f97cf13e61f
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-28 22:26:07 -0300

    fix: elimino CrearUsuarioDTO

--- Compartido/DTOs/Usuarios/UsuarioDTO.cs
+++ Compartido/DTOs/Usuarios/UsuarioDTO.cs
@@ -6,30 +6,6 @@ using System.Threading.Tasks;
 
 namespace Compartido.DTOs.Usuarios
 {
-    /*
-        /// <summary>
-        /// DTO para CREAR un usuario.
-        /// Lo usa el frontend cuando manda un POST /api/usuarios.
-        /// Incluye la contraseña porque es necesaria solo al momento de crear.
-        /// </summary>
-        public class CrearUsuarioDTO
-        {
-            public string Nombre { get; set; } = string.Empty;
-            public string Apellido { get; set; } = string.Empty;
-            public string Email { get; set; } = string.Empty;
-
-            // Se recibe en texto plano y el servicio se encarga de hashearla
-            public string Password { get; set; } = string.Empty;
-
-            // Se recibe el ID del rol (ej: 1 = Despachante, 2 = Asistente, 3 = Cliente)
-            public int RolId { get; set; }
-        }
-
-        /// <summary>
-        /// DTO para MODIFICAR un usuario.
-        /// Lo usa el frontend cuando manda un PUT /api/usuarios/{id}.
-        /// No incluye contraseña porque modificarla será un proceso aparte.
-        /// </summary>*/
         public class ModificarUsuarioDTO
         {
             public string Nombre { get; set; } = string.Empty;
