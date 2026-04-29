ed13d7e9063e94cf98b4fc60505d21791eb739b4
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-29 00:21:01 -0300

    feat: creao exception de ya existe rol

---
+++ LogicaNegocio/Excepciones/Roles/YaExisteRolConEseNombreException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Roles
+{
+    public class YaExisteRolConEseNombreException : ClassException
+    {
+        public YaExisteRolConEseNombreException() : base("Ya existe un rol con ese nombre.") { }
+    }
+}
