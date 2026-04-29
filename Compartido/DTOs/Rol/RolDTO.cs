e26cb8d3db5e2434f7ac954c02d55b361603ae77
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-28 23:14:25 -0300

    feat: creo crear, modificar y respuesta DTO de rol

---
+++ Compartido/DTOs/Rol/RolDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compartido.DTOs.Rol
+{
+    public class RolDTO
+    {
+        public class CrearRolDTO
+        {
+            // Lo que el frontend manda para CREAR un rol
+            public string NombreRol { get; set; } = string.Empty;
+        }
+
+        // Lo que el frontend manda para MODIFICAR un rol
+        public class ModificarRolDTO
+        {
+            public string NombreRol { get; set; } = string.Empty;
+        }
+
+        // Lo que la API DEVUELVE al frontend
+        public class RolRespuestaDTO
+        {
+            public int Id { get; set; }
+            public string NombreRol { get; set; } = string.Empty;
+        }
+    }
+}
