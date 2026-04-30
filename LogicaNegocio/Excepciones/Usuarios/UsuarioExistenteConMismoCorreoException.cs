a8311a243e9be37128c51f88568ffbd35a75bf36
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-30 01:18:23 -0300

    feat: creo excepciones y sus mensajes

---
+++ LogicaNegocio/Excepciones/Usuarios/UsuarioExistenteConMismoCorreoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Usuarios
+{
+    public class UsuarioExistenteConMismoCorreoException : ClassException
+    {
+        public UsuarioExistenteConMismoCorreoException() : base("Ya existe un usuario con ese correo electrónico.") { }
+    }
+}
