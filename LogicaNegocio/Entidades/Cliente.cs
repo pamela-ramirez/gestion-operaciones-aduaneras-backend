3467d3bf636aa7868707914c82ed397e78f8bd0a
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-30 00:52:41 -0300

    feat: agrego metodos de validacion

--- LogicaNegocio/Entidades/Cliente.cs
+++ LogicaNegocio/Entidades/Cliente.cs
@@ -1,4 +1,6 @@
-﻿using LogicaNegocio.ValueObject;
+﻿using LogicaNegocio.Excepciones;
+using LogicaNegocio.Excepciones.Cliente;
+using LogicaNegocio.ValueObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +11,32 @@ namespace LogicaNegocio.Entidades
 {
     public class Cliente:Usuario
     {
-        public string Rut { get; set; } = string.Empty;
+        public Rut Rut { get; set; }
         public string Telefono { get; set; } = string.Empty;
         public string? Direccion { get; set; }
 
 
         public Cliente() { }
 
-        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, string rut, string telefono, string? direccion) : base(nombre, apellido, email, password, rol)
+        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, Rut rut, string telefono, string? direccion) : base(nombre, apellido, email, password, rol)
         {
             this.Rut = rut;
             this.Telefono = telefono;
             this.Direccion = direccion;
+            ValidarCliente();
         }
 
+        public void ValidarCliente()
+        {
+            if (string.IsNullOrWhiteSpace(Telefono))
+                throw new ClienteTelefonoVacioException();
+
+            if (Telefono.Length > 20)
+                throw new ClienteTelefonoMasVeinteCaracteresException();
+
+            if (Direccion != null && Direccion.Length > 300)
+                throw new ClienteDireccionExcesoCaracteresException();
+        }
 
     }
 }
