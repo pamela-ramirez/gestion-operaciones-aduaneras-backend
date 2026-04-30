c67bfc4c825a279fbca8987f514572896480c7d8
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-30 00:57:32 -0300

    feat: agrego otra exception de validacion

---
+++ LogicaNegocio/Excepciones/Roles/RolExcesoCaracteresException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Roles
+{
+    public class RolExcesoCaracteresException : ClassException
+    {
+        public RolExcesoCaracteresException() : base("El nombre del rol no puede superar 100 caracteres.") { }
+    }
+}
