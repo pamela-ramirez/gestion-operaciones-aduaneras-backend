568263c9b2ed4912e32cd85252632243219a2bd7
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-04-30 21:58:11 -0300

    feat: agrego excepciones utilizada en rol y usuario

---
+++ LogicaNegocio/Excepciones/Roles/RolDebeSerAsignadoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Roles
+{
+    public class RolDebeSerAsignadoException : ClassException
+    {
+        public RolDebeSerAsignadoException() : base("Debe asignar un rol al usuario.")
+        {
+        }
+    }
+}
