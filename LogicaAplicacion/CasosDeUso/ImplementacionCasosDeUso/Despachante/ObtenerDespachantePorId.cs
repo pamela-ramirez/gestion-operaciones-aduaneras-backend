417367a3c1a4762862470deb63e64ecb831460d0
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-02 18:39:56 -0300

    feat: agrego interfaz y CU obtenerPorId de despachante

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Despachante/ObtenerDespachantePorId.cs
@@ -0,0 +1,38 @@
+﻿using Compartido.DTOs.Despachante;
+using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Despachante;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Despachante
+{
+    public class ObtenerDespachantePorId : IObtenerDespachantePorId
+    {
+        private readonly IRepositorioDespachante _repositorioDespachante;
+
+        public ObtenerDespachantePorId(IRepositorioDespachante repositorioDespachante)
+        {
+            _repositorioDespachante = repositorioDespachante;
+        }
+        public DespachanteDTO Ejecutar(int id)
+        {
+            var despachante = _repositorioDespachante.FindById(id);
+            if (despachante == null)
+            {
+                throw new Exception("Despachante no encontrado");
+            }
+            return new DespachanteDTO 
+            {
+                Nombre = despachante.Nombre,
+                Apellido = despachante.Apellido,
+                Email = despachante.Email.Valor,
+                Codigo = despachante.Codigo
+            };
+
+            // TODO Pasar el new DespachanteDTO a un mapeador para mapearlo a un DTO
+        }
+    }
+}
---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/Despachante/IObtenerDespachantePorId.cs
@@ -0,0 +1,14 @@
+﻿using Compartido.DTOs.Despachante;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Despachante
+{
+    public interface IObtenerDespachantePorId
+    {
+        DespachanteDTO Ejecutar(int id);
+    }
+}
