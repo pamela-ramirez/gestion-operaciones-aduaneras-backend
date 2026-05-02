bb15f40edacef636870838bf74086d98767c65d3
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-02 17:48:05 -0300

    feat: se crea dto de despachante

---
+++ Compartido/DTOs/Despachante/DespachanteDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compartido.DTOs.Despachante
+{
+    public class DespachanteDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Codigo { get; set; } = string.Empty;
+    }
+
+    public class CrearDespachanteDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+    }
+
+    public class CrearDespachanteRespuestaDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string PasswordTemporal { get; set; } = string.Empty;
+    }
+}
