5f3e9dd276be5983a59d3cc170a67ac7046ccdd0
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-05 22:10:26 -0300

    feature: agrego excepciones de razon social cliente

---
+++ LogicaNegocio/Excepciones/Cliente/RazonSocialVaciaException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Cliente
+{
+    public class RazonSocialVaciaException : ClassException
+    {
+        public RazonSocialVaciaException()  : base("La razón social no puede estar vacía.") { }
+    }
+}
