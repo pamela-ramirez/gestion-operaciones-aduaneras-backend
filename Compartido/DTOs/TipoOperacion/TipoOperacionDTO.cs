3768fa7415c2a79913928f20d5096ff66fe17946
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-07 01:12:22 -0300

    feature: reubico dto de tipo operacion y añado logica

--- Compartido/DTOs/TipoOperacion/TipoOperacionDTO.cs
+++ Compartido/DTOs/TipoOperacion/TipoOperacionDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Compartido.DTOs
+namespace Compartido.DTOs.TipoOperacion
 {
     // DTO para CREAR un tipo de operación.
     public class CrearTipoOperacionDTO
