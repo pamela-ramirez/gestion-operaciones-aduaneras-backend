f4afffc6d57afd70f44bcc59e2a5b09dfbf562dd
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-07 17:07:55 -0300

    feature: elimino excepcion de cliente, ya que pertenece a LogicaAplicacion

--- LogicaNegocio/Excepciones/Cliente/ClienteConOperacionesActivasException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LogicaNegocio.Excepciones.Cliente
-{
-    public class ClienteConOperacionesActivasException : ClassException
-    {
-        public ClienteConOperacionesActivasException() : base("El cliente no puede ser eliminado porque tiene operaciones activas.") { }
-    }
-}
