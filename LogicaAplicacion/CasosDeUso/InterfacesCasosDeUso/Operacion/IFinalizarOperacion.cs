d937a01ad707082e43cf2d4cd2ba73baabfadf33
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-06 22:26:08 -0300

    feature: agrego interfaces de casos de uso operaciones

---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/Operacion/IFinalizarOperacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.Operacion.OperacionDTO;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion
+{
+    public interface IFinalizarOperacion
+    {
+        OperacionRespuestaDTO Ejecutar(int operacionId);
+    }
+}
---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/Operacion/IObtenerOperaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.Operacion.OperacionDTO;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion
+{
+    public interface IObtenerOperaciones
+    {
+        IEnumerable<OperacionListadoDTO> Ejecutar(
+           int? clienteId,
+           int? tipoOperacionId,
+           string? estado,
+           DateTime? fechaDesde,
+           DateTime? fechaHasta);
+    }
+}
