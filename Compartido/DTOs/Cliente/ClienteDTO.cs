2dcf10a2c3c38f0ab003b25ab70246e68d07ccac
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-07 20:50:52 -0300

    fix: agrego id en el clienteDTO que muestra los datos de cliente en la respuesta

--- Compartido/DTOs/Cliente/ClienteDTO.cs
+++ Compartido/DTOs/Cliente/ClienteDTO.cs
@@ -8,6 +8,7 @@ namespace Compartido.DTOs.Cliente
 {
     public class ClienteDTO
     {
+        public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string Apellido { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
