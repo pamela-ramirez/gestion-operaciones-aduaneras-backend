f57846a8bdee2bcbf5d36418225d64ef066eb884
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-06 22:35:14 -0300

    feature: creo repositorio de tipo operacion

---
+++ LogicaAccesoDatos/Repositorios/RepositorioTipoOperacion.cs
@@ -0,0 +1,46 @@
+﻿using LogicaAccesoDatos.Contexto;
+using LogicaNegocio.Entidades;
+using LogicaNegocio.InterfacesRepositorios;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Repositorios
+{
+    public class RepositorioTipoOperacion : IRepositorioTipoOperacion
+    {
+        private readonly GestionOperacionesDbContext _context;
+
+        public RepositorioTipoOperacion(GestionOperacionesDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(TipoOperacion item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TipoOperacion> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoOperacion FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TipoOperacion item, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
