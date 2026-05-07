2674ec9f88062887915a0b29d3f47b19c8855809
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-06 22:58:10 -0300

    feat: obtener las operaciones de un cliente

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Operacion/ObtenerOperacionesPorCliente.cs
@@ -0,0 +1,39 @@
+﻿using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion;
+using LogicaAplicacion.Mappers;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.Operacion.OperacionDTO;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Operacion
+{
+    public class ObtenerOperacionesPorCliente : IObtenerOperacionesPorCliente
+    {
+        private readonly IRepositorioOperacion _operacionRepo;
+        private readonly IRepositorioCliente _clienteRepo;
+
+        public ObtenerOperacionesPorCliente(
+            IRepositorioOperacion operacionRepo,
+            IRepositorioCliente clienteRepo)
+        {
+            _operacionRepo = operacionRepo;
+            _clienteRepo = clienteRepo;
+        }
+
+        public IEnumerable<OperacionListadoDTO> Ejecutar(int clienteId)
+        {
+            // Verificamos que el cliente exista antes de buscar sus operaciones
+            var cliente = _clienteRepo.FindById(clienteId);
+            if (cliente == null)
+                throw new Exception("Cliente no encontrado.");
+
+            return _operacionRepo
+                .FindByClienteId(clienteId)
+                .Select(OperacionMapper.ToListadoDTO)
+                .ToList();
+        }
+    }
+}
