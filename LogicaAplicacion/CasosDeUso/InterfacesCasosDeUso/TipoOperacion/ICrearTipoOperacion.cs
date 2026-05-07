bd518911647c841359908e2b534e5f06f63ff23c
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-07 01:10:50 -0300

    feature: interfaces de CU de tipo operacion

---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/TipoOperacion/ICrearTipoOperacion.cs
@@ -0,0 +1,14 @@
+﻿using Compartido.DTOs.TipoOperacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.TipoOperacion
+{
+    public interface ICrearTipoOperacion
+    {
+        TipoOperacionListadoDTO Ejecutar(CrearTipoOperacionDTO dto);
+    }
+}
---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/TipoOperacion/IObtenerTipoOperacionPorId.cs
@@ -0,0 +1,15 @@
+﻿using Compartido.DTOs.Operacion;
+using Compartido.DTOs.TipoOperacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.TipoOperacion
+{
+    public interface IObtenerTipoOperacionPorId
+    {
+        TipoOperacionListadoDTO Ejecutar(int id);
+    }
+}
