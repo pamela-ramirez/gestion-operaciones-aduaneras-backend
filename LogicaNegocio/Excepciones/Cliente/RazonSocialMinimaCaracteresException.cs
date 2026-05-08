6334310451e01eff7e90025617637ade1935b4ee
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 20:48:08 -0300

    feature: agrego excepcion de cliente

---
+++ LogicaNegocio/Excepciones/Cliente/RazonSocialMinimaCaracteresException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Cliente
+{
+    public class RazonSocialMinimaCaracteresException : ClassException
+    {
+        public RazonSocialMinimaCaracteresException()
+            : base("La razón social debe tener al menos 2 caracteres.") { }
+    }
+}
