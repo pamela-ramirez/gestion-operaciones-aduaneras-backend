d60e9a4fb439ca01134efa78ee67db30bbb7c9ad
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 20:48:27 -0300

    feature: agrego excepcion de usuario

---
+++ LogicaNegocio/Excepciones/Usuarios/EstadoInvalidoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Usuarios
+{
+    public class EstadoInvalidoException : ClassException
+    {
+        public EstadoInvalidoException() : base("El estado del usuario es inválido.") { }
+    }
+}
---
+++ LogicaNegocio/Excepciones/Usuarios/FechaCreacionInvalidaException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Usuarios
+{
+    public class FechaCreacionInvalidaException: ClassException
+    {
+        public FechaCreacionInvalidaException() : base("La fecha de creación es inválida.")
+        {
+        }
+    }
+}
