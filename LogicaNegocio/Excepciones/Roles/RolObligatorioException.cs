0704fbd4765a1dda52af99b5ec2b4e970b546597
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 20:48:17 -0300

    feature: agrego excepcion de rol

---
+++ LogicaNegocio/Excepciones/Roles/RolObligatorioException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Roles
+{
+    public class RolObligatorioException : ClassException
+    {
+        public RolObligatorioException() : base("El rol es obligatorio.")
+        {
+        }
+    }
+}
