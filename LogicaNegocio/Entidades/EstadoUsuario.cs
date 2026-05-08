b93395544f243c9d0036069903032af6060558c2
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 20:50:02 -0300

    feature: creo enum estado usuario con pendiente, activo, inactivo para usarlo despues

---
+++ LogicaNegocio/Entidades/EstadoUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Entidades
+{
+    public enum EstadoUsuario
+    {
+        Pendiente = 1,
+        Activo = 2,
+        Inactivo = 3
+    }
+}
