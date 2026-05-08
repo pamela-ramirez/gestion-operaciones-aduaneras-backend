a69b5b83169ae390c71397304e75eff8107890f7
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-07 21:09:36 -0300

    fix: modifico string Rut por Rut Rut por value object

--- LogicaNegocio/Entidades/Cliente.cs
+++ LogicaNegocio/Entidades/Cliente.cs
@@ -9,7 +9,7 @@ namespace LogicaNegocio.Entidades
 {
     public class Cliente:Usuario
     {
-        public string Rut { get; set; } = string.Empty;
+        public Rut Rut { get; set; }
         public string RazonSocial { get; set; } = string.Empty;
         public string? Telefono { get; set; } = string.Empty;
         public string? Direccion { get; set; }
@@ -17,7 +17,7 @@ namespace LogicaNegocio.Entidades
 
         public Cliente() { }
 
-        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, string rut, string razonSocial, string telefono, string? direccion) : base(nombre, apellido, email, password, rol)
+        public Cliente(string nombre, string apellido, Email email, Password password, Rol rol, Rut rut, string razonSocial, string telefono, string? direccion) : base(nombre, apellido, email, password, rol)
         {
             this.Rut = rut;
             this.RazonSocial = razonSocial; 
