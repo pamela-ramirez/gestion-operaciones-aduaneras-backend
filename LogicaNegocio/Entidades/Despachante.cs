4fdcf9035859b81fe87f42f1c15872d41113592e
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 21:11:43 -0300

    feature: agrego metodo validar y añado string? codigo al constructor de despachante

--- LogicaNegocio/Entidades/Despachante.cs
+++ LogicaNegocio/Entidades/Despachante.cs
@@ -1,4 +1,5 @@
-﻿using LogicaNegocio.ValueObject;
+﻿using LogicaNegocio.Excepciones.Despachante;
+using LogicaNegocio.ValueObject;
 
 namespace LogicaNegocio.Entidades
 {
@@ -7,14 +8,21 @@ namespace LogicaNegocio.Entidades
         //codigo de despachante
         public string? Codigo { get; set; }
 
-
         public Despachante() { }
 
-        public Despachante(string nombre, string apellido, Email email, Password password, Rol rol) : base(nombre, apellido, email, password, rol)
+        public Despachante(string nombre, string apellido, Email email, Password password, Rol rol, string? codigo) : base(nombre, apellido, email, password, rol)
         {
-
+            this.Codigo = codigo;
+            Validar();
         }
 
+        public override void Validar()
+        {
+            // valida todo lo heredado de Usuario
+            base.Validar();
 
+            if (Codigo.Length > 30)
+                throw new CodigoDespachanteExcesoCaracteresException();
+        }
     }
 }
