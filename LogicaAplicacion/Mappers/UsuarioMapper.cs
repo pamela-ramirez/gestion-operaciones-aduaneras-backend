875c86447641a6f0947d5bd7e499b44ecc725643
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 22:46:34 -0300

    feature: creo mapper de usuario porque lo utilizao en ObetenusuarioLogueado

---
+++ LogicaAplicacion/Mappers/UsuarioMapper.cs
@@ -0,0 +1,25 @@
+﻿using Compartido.DTOs.Usuarios;
+using LogicaNegocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Mappers
+{
+    public static class UsuarioMapper
+    {
+        public static UsuarioLogueadoDTO ToLogueadoDTO(Usuario usuario)
+        {
+            return new UsuarioLogueadoDTO
+            {
+                Id = usuario.Id,
+                Email = usuario.Email.Valor,
+                Rol = usuario.Rol.NombreRol,
+                Estado = usuario.Estado,
+                PrimerLogin = usuario.PrimerLogin
+            };
+        }
+    }
+}
