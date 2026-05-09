3967e273cc9f1a5e135081a03c3b6cc785e6296f
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 21:12:30 -0300

    feature: agrego exception de codigo de despachante

---
+++ LogicaNegocio/Excepciones/Despachante/CodigoDespachanteExcesoCaracteresException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Despachante
+{
+    public class CodigoDespachanteExcesoCaracteresException : ClassException
+    {
+        public CodigoDespachanteExcesoCaracteresException()
+            : base("El código del despachante no puede superar los 30 caracteres.") { }
+    }
+}
