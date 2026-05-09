43c5714bf10548dce31b80d8953238234bfd7682
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 21:22:40 -0300

    feature: agrego exception de fecha registro operacion

---
+++ LogicaNegocio/Excepciones/Operacion/FechaRegistroOperacionInvalidaException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Operacion
+{
+    public class FechaRegistroOperacionInvalidaException : ClassException
+    {
+        public FechaRegistroOperacionInvalidaException() : base("La fecha de registro de la operación es incorrecta.")
+        {
+        }
+    }
+}
