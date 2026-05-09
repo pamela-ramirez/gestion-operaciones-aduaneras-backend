55454b3c14027e967cdbd2ccc5bc44447194a327
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 23:41:51 -0300

    feature: agrego excepciones personalizadas de operacion

---
+++ LogicaAplicacion/Excepciones/Operacion/ClienteSeleccionadoNoExisteException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Excepciones.Operacion
+{
+    public class ClienteSeleccionadoNoExisteException : ClaseExcepcion
+    {
+        public ClienteSeleccionadoNoExisteException() : base("El cliente seleccionado no existe.")
+        {
+        }
+    }
+}
