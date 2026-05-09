77de09bf659c4795cc3b2459424f6d580066ba83
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 23:57:30 -0300

    feature: añado interfaz y CU de obtener operaciones

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Operacion/ObtenerOperaciones.cs
@@ -0,0 +1,30 @@
+﻿using Compartido.DTOs.Operacion;
+using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion;
+using LogicaAplicacion.Mappers;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Operacion
+{
+    public class ObtenerOperaciones : IObtenerOperaciones
+    {
+        private readonly IRepositorioOperacion _operacionRepo;
+
+        public ObtenerOperaciones(IRepositorioOperacion operacionRepo)
+        {
+            _operacionRepo = operacionRepo;
+        }
+
+        public IEnumerable<OperacionListadoDTO> Ejecutar()
+        {
+            var operaciones = _operacionRepo.FindAll();
+            return operaciones
+                .Select(OperacionMapper.ToListadoDTO)
+                .ToList();
+        }
+    }
+}
---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/Operacion/IObtenerOperaciones.cs
@@ -0,0 +1,14 @@
+﻿using Compartido.DTOs.Operacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion
+{
+    public interface IObtenerOperaciones
+    {
+        IEnumerable<OperacionListadoDTO> Ejecutar();
+    }
+}
