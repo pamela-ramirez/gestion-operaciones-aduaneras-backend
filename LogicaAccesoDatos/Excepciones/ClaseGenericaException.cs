15f87e695fe0c5de31fed0db08731df15f5961d8
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 22:01:05 -0300

    feature: añado excepcion generica de LogicaAccesoDatos

---
+++ LogicaAccesoDatos/Excepciones/ClaseGenericaException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Excepciones
+{
+    public class ClaseGenericaException : Exception
+    {
+        public ClaseGenericaException() { }
+
+        public ClaseGenericaException(string message) : base(message) { }
+
+        public ClaseGenericaException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
