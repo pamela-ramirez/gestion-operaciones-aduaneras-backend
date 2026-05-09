609d4d316532d3209b1030b5425387895b333372
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 21:47:06 -0300

    fix: paso codigo de crear dto manual al mapper

--- LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Cliente/ObtenerClientes.cs
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Cliente/ObtenerClientes.cs
@@ -1,5 +1,6 @@
 ﻿using Compartido.DTOs.Cliente;
 using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Cliente;
+using LogicaAplicacion.Mappers;
 using LogicaNegocio.InterfacesRepositorios;
 
 namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Cliente
@@ -17,18 +18,7 @@ namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Cliente
         public IEnumerable<ClienteDTO> Ejecutar()
         {
             var clientes = _clienteRepo.FindAll();
-            return clientes.Select(c => new ClienteDTO
-            {
-                Id = c.Id,
-                Nombre = c.Nombre,
-                Apellido = c.Apellido,
-                Email = c.Email.Valor,
-                RazonSocial = c.RazonSocial,
-                Rut = c.Rut.Valor,
-                Telefono = c.Telefono,
-                Direccion = c.Direccion,
-                Estado = c.Estado
-            }).ToList();
+            return clientes.Select(c => ClienteMapper.ToClienteDTO(c)).ToList();
         }
     }
 }
