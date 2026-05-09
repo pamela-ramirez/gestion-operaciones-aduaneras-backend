e642c876ced986203bdcb7ee207d28d02f8777f6
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 23:53:57 -0300

    feature: añado exceptions personalizadas de fecha y op no encontrada

---
+++ LogicaAplicacion/Excepciones/Operacion/FechasIntercambiadasException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Excepciones.Operacion
+{
+    public class FechasIntercambiadasException: ClaseExcepcion
+    {
+        public FechasIntercambiadasException() : base("La fecha desde no puede ser mayor que la fecha hasta.")
+        {
+        }
+    }
+}
---
+++ LogicaAplicacion/Excepciones/Operacion/OperacionNoEncontradaException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Excepciones.Operacion
+{
+    public class OperacionNoEncontradaException : ClaseExcepcion
+    {
+        public OperacionNoEncontradaException() : base("La operación solicitada no se encontró.")
+        {
+    }
+}
