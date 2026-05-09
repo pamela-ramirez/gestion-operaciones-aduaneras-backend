636d8f3be5ff0f27969306fd43efb1679637a857
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-08 23:28:46 -0300

    feature: añado excepcion rol

---
+++ LogicaAplicacion/Excepciones/Rol/NombreRolNuloException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Excepciones.Rol
+{
+    public class NombreRolNuloException : ClaseExcepcion
+    {
+        public NombreRolNuloException() : base("El nombre del rol no puede ser nulo.")
+        {
+        }
+    }
+}
