1bcb562a21f519d5af5a2a1c5b7989487445d310
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-10 00:05:31 -0300

    feature: añado CU e interfaz de obtener todos los tipos de conocimiento

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/TiposConocimiento/ObtenerTiposConocimiento.cs
@@ -0,0 +1,28 @@
+﻿using Compartido.DTOs.TipoConocimiento;
+using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.TiposConocimiento;
+using LogicaAplicacion.Mappers;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.TipoConocimiento.TipoConocimientoDTO;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.TiposConocimiento
+{
+    public class ObtenerTiposConocimiento : IObtenerTiposConocimiento
+    {
+        private readonly IRepositorioTipoConocimiento _repositorioTipoConocimiento;
+
+        public ObtenerTiposConocimiento(IRepositorioTipoConocimiento repositorioTipoConocimiento)
+        {
+            _repositorioTipoConocimiento = repositorioTipoConocimiento;
+        }
+        public IEnumerable<TipoConocimientoListadoDTO> Ejecutar()
+        {
+            var tiposConocimiento = _repositorioTipoConocimiento.FindAll();
+            return TipoConocimientoMapper.ToListaDTO(tiposConocimiento); 
+        }
+    }
+}
---
+++ LogicaAplicacion/CasosDeUso/InterfacesCasosDeUso/TiposConocimiento/IObtenerTiposConocimiento.cs
@@ -0,0 +1,15 @@
+﻿using Compartido.DTOs.TipoConocimiento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compartido.DTOs.TipoConocimiento.TipoConocimientoDTO;
+
+namespace LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.TiposConocimiento
+{
+    public interface IObtenerTiposConocimiento
+    {
+        IEnumerable<TipoConocimientoListadoDTO> Ejecutar();
+    }
+}
