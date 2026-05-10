0df011df74fa2b71b4d98d47553869b2f9fc16c1
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-10 18:33:51 -0300

    feature: CU de finalizar operacion

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Operacion/FinalizarOperacion.cs
@@ -0,0 +1,33 @@
+﻿using Compartido.DTOs.Operacion;
+using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion;
+using LogicaAplicacion.Excepciones.Operacion;
+using LogicaAplicacion.Mappers;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Operacion
+{
+    public class FinalizarOperacion : IFinalizarOperacion
+    {
+        private readonly IRepositorioOperacion _operacionRepositorio;
+
+        public FinalizarOperacion(IRepositorioOperacion operacionRepositorio)
+        {
+            _operacionRepositorio = operacionRepositorio;
+        }
+        public OperacionRespuestaDTO Ejecutar(int operacionId)
+        {
+            var operacion = _operacionRepositorio.FindById(operacionId);
+            if (operacion == null)
+
+                throw new OperacionNoEncontradaException();
+
+            operacion.Finalizar();
+            return OperacionMapper.ToDTO(operacion);
+        }
+    }
+}
