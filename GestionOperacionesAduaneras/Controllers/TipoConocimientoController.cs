89c33608c3134f973b0013ae65156b3428fea205
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-10 00:06:06 -0300

    feature: añado controller de tipo de conocimiento y el metodo de obtener todos

---
+++ GestionOperacionesAduaneras/Controllers/TipoConocimientoController.cs
@@ -0,0 +1,35 @@
+﻿using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.TiposConocimiento;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionOperacionesAduaneras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TipoConocimientoController : ControllerBase
+    {
+        private readonly IObtenerTiposConocimiento _obtenerTiposConocimiento;
+
+        public TipoConocimientoController(IObtenerTiposConocimiento obtenerTiposConocimiento)
+        {
+            _obtenerTiposConocimiento = obtenerTiposConocimiento;
+        }
+
+        // GET /api/tipoconocimiento
+        [HttpGet]
+        [Authorize(Roles = "Admin, Despachante")]
+        public IActionResult ObtenerTodos()
+        {
+            try
+            {
+                var resultado = _obtenerTiposConocimiento.Ejecutar();
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+        }
+    }
+}
