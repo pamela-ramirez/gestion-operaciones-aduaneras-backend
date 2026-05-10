6f5b625914709a25a527d108a6763f72212a3277
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-09 21:42:10 -0300

    feature: añado CU actualizar operacion con atributos opcionales

---
+++ LogicaAplicacion/CasosDeUso/ImplementacionCasosDeUso/Operacion/ActualizarOperacion.cs
@@ -0,0 +1,48 @@
+﻿using Compartido.DTOs.Operacion;
+using LogicaAplicacion.CasosDeUso.InterfacesCasosDeUso.Operacion;
+using LogicaAplicacion.Excepciones.Operacion;
+using LogicaAplicacion.Excepciones.TipoConocimiento;
+using LogicaAplicacion.Mappers;
+using LogicaNegocio.Entidades;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosDeUso.ImplementacionCasosDeUso.Operacion
+{
+    public class ActualizarOperacion : IActualizarOperacion
+    {
+        private readonly IRepositorioOperacion _operacionRepo;
+        private readonly IRepositorioTipoConocimiento _tipoConocimientoRepo;
+
+        public ActualizarOperacion(IRepositorioOperacion operacionRepo, IRepositorioTipoConocimiento tipoConocimientoRepo)
+        {
+            _operacionRepo = operacionRepo;
+            _tipoConocimientoRepo = tipoConocimientoRepo;
+        }
+        public OperacionRespuestaDTO Ejecutar(int operacionId, ActualizarDatosAduanerosDTO dto)
+        {
+            var operacion = _operacionRepo.FindById(operacionId);
+            if (operacion == null)
+                throw new OperacionNoEncontradaException();
+
+            TipoConocimiento? tipoConocimiento = null;
+            if (dto.TipoConocimientoId.HasValue)
+            {
+                tipoConocimiento = _tipoConocimientoRepo.FindById(dto.TipoConocimientoId.Value);
+                if (tipoConocimiento == null)
+                    throw new TipoConocimientoNoEncontradoException();
+            }
+
+            // Delegar la lógica a la entidad (valida y actualiza su estado)
+            operacion.ActualizarDatosAduaneros(dto.NroDua, tipoConocimiento, dto.NroConocimiento);
+
+            _operacionRepo.Update(operacion, operacionId);
+
+            return OperacionMapper.ToDTO(operacion);
+        }
+    }
+}
