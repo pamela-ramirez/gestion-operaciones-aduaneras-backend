33dffdd520304f705f6bb9b635c885f103482275
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-09 21:42:50 -0300

    feature: excepcion de tipo conoc no encontrado de LogicaAplicacion

---
+++ LogicaAplicacion/Excepciones/TipoConocimiento/TipoConocimientoNoEncontradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Excepciones.TipoConocimiento
+{
+    public class TipoConocimientoNoEncontradoException : ClaseExcepcion
+    {
+        public TipoConocimientoNoEncontradoException() : base("No se encontró el tipo de conocimiento.")
+        {
+        }
+    }
+}
