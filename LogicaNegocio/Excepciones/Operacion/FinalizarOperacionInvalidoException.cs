6880643a4844aa212a747336a17c92e3bc57c94f
RominaRamon <RR319999@fi365.ort.edu.uy>
2026-05-10 18:09:53 -0300

    feature: añado exception de finalizar operacion invalida

---
+++ LogicaNegocio/Excepciones/Operacion/FinalizarOperacionInvalidoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Operacion
+{
+    public class FinalizarOperacionInvalidoException : ClassException
+    {
+        public FinalizarOperacionInvalidoException() : 
+            base("No se puede finalizar la operación. Asegúrese de que todos los datos aduaneros estén completos y que la operación esté en proceso.")
+        {
+        }
+    }
+}
